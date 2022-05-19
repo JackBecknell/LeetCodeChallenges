3d399d81021133ea17a125c35208cc76575e8c1e
JackBecknell <52moosefit@gmail.com>
2022-05-19 17:02:17 -0400

    For integer to roman numeral I used a handler function 'handle_int_to_roman_str' which takes an int as a param, then calls recursive function int_to_roman_list which takes the integer to be assesed, and parameter 'recursive_roman_list' which is default set to false. This function assesses the integer being passed in via if else statment for every unique number identifyer between 1 and 1000 in the roman numeral dict, and adds the correct Char set on top of the return_value, assesses if it is equal to zero and if it isn't it calls itself with the remainder, or if it is returns a list of chars to handler function which compacts it to one string and finally returns it to the user.

--- mediumChallenges/integerToRomanNums.py
+++ mediumChallenges/integerToRomanNums.py
@@ -1,28 +1,61 @@
-# unfinished
+# FINISHED
 
-def int_to_roman(integer):
-    return_value = []
-    working_value = integer
-    remainder = 0
-    if working_value >= 1000:
-        remainder = working_value%1000
-        roman_m = int(working_value/1000)
-        for i in range(roman_m):
-            return_value.append("M")
-        if remainder == 0:
-            return return_value
-        else: 
-            working_value = remainder
-    if working_value >= 500:
-        remainder = working_value%500
-        roman_d = int(working_value/500)
-        for i in range(roman_d):
-            return_value.append("D")
-        if remainder == 0:
-            return return_value
-        else: 
-            working_value = remainder
+# This recursive function will call itself as many times as it takes until the integer being passed through it has finally reached zero
+# and the return array is complete.
+def int_to_roman_list(integer, recursive_roman_list=False):
+    if recursive_roman_list:
+        return_value = recursive_roman_list
+    else:
+        return_value = []
+    if integer >= 1000:
+        return_value.extend(['M'])
+        integer -= 1000
+    elif integer >= 900:
+        return_value.extend(['C','M'])
+        integer -= 900
+    elif integer >= 500:
+        return_value.extend(['D'])
+        integer -= 500
+    elif integer >= 400:
+        return_value.extend(['C','D'])
+        integer -= 400
+    elif integer >= 100:
+        return_value.extend(['C'])
+        integer -= 100
+    elif integer >= 90:
+        return_value.extend(['X','C'])
+        integer -= 90
+    elif integer >= 50:
+        return_value.extend(['L'])
+        integer -= 50
+    elif integer >= 40:
+        return_value.extend(['X','L'])
+        integer -= 40
+    elif integer >= 10:
+        return_value.extend(['X'])
+        integer -= 10
+    elif integer >= 9:
+        return_value.extend(['I','X'])
+        integer -= 9
+    elif integer >= 5:
+        return_value.extend(['V'])
+        integer -= 5
+    elif integer >= 4:
+        return_value.extend(['I', 'V'])
+        integer -= 4
+    elif integer >= 1:
+        return_value.extend(['I'])
+        integer -= 1
+    if integer != 0:
+        return_value = int_to_roman_list(integer, return_value)
+    return return_value
 
+def handle_int_to_roman_str(int):
+    list_of_roman_chars = int_to_roman_list(int)
+    return_value = ""
+    for char in list_of_roman_chars:
+        return_value += char
+    return return_value
 
-value = int_to_roman(3500)
+value = handle_int_to_roman_str(3424)
 print(value)
